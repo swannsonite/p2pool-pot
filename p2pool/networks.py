257074d0a622115142ce81a5078c94a03f753515
swannsonite <swannsonite@gmail.com>
2014-04-03 08:31:55 -0500

    Update networks.py
    Added potcoin math
    Added pot.hashfire.us to bootstrap

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -143,6 +143,26 @@ nets = dict(
         VERSION_CHECK=lambda v: True,
         VERSION_WARNING=lambda v: 'Upgrade Fastcoin to >= 0.8.5.1!' if v < 70002 else None,
     ),
+    potcoin=math.Object(
+        PARENT=networks.nets['potcoin'],
+        SHARE_PERIOD=10, # seconds target spacing
+        NEW_SHARE_PERIOD=10, # seconds target spacing
+        CHAIN_LENGTH=24*60*60//10, # shares
+        REAL_CHAIN_LENGTH=24*60*60//10, # shares
+        TARGET_LOOKBEHIND=200, # shares coinbase maturity
+        SPREAD=10, # blocks
+        NEW_SPREAD=10, # blocks
+        IDENTIFIER='DDA1A1D3B2F68CDD'.decode('hex'),
+        PREFIX='A2C3D4D541C11DDD'.decode('hex'),
+        P2P_PORT=8420,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=False,
+        WORKER_PORT=9420,
+        BOOTSTRAP_ADDRS='us-east1.cryptovein.com pot.hashfire.us'.split(' '),
+        ANNOUNCE_CHANNEL='#cryptovein',
+        VERSION_CHECK=lambda v: True,
+    ),
 
 )
 for net_name, net in nets.iteritems():
