caf5e963bb10bfacc98cf0a783f9341c46c8fd76
swannsonite <swannsonite@gmail.com>
2014-04-30 00:19:09 -0500

    Update networks.py
    Diamond

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -163,7 +163,25 @@ nets = dict(
         ANNOUNCE_CHANNEL='#cryptovein',
         VERSION_CHECK=lambda v: True,
     ),
-
+    diamond=math.Object(
+        P2P_PREFIX='fbc0b6db'.decode('hex'),
+        P2P_PORT=17771,
+        ADDRESS_VERSION=35,
+        RPC_PORT=17772,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'diamondaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 1*10000000 >> (height + 1)//4204800,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=60, # s targetspacing
+        SYMBOL='DMD',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'diamond') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/diamond/')  if platform.system() == 'Darwin' else os.path.expanduser('~/.diamond'), 'diamond.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://d.evco.in/abe/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://d.evco.in/abe/address/',
+        SANE_TARGET_RANGE=(2**256//100000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+    ),
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name
